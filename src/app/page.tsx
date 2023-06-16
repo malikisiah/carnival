ee6ab8536d54b28828745c89bb9a0cae86bb4a6d
Malik Teague <malikisiah214@gmail.com>
2023-06-16 14:30:56 -0700

    working on navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
+import NavBar from "./navbar";
 export default function Home() {
   return (
-    <div
-      className='hero min-h-screen'
-      // style={{
-      //   backgroundImage: `url("https://w.wallhaven.cc/full/xl/wallhaven-xlrp3z.jpg")`,
-      // }}
-    >
-      <div className='hero-overlay bg-opacity-60'></div>
-      <div className='hero-content text-center text-neutral-content'>
-        <div className='max-w-md'>
-          <h1 className='mb-5 text-5xl font-bold'>Project Carnival</h1>
+    <div>
+      <NavBar />
+      <div
+        className='hero min-h-screen'
+        // style={{
+        //   backgroundImage: `url("https://w.wallhaven.cc/full/xl/wallhaven-xlrp3z.jpg")`,
+        // }}
+      >
+        <div className='hero-overlay bg-opacity-60'></div>
+        <div className='hero-content text-center text-neutral-content'>
+          <div className='max-w-md'>
+            <h1 className='mb-5 text-5xl font-bold'>Project Carnival</h1>
 
-          <p className='mb-5'></p>
+            <p className='mb-5'></p>
+          </div>
         </div>
       </div>
     </div>
