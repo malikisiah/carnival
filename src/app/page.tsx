3cb805b1bb08a61934e71b208be76b4cd0417b82
Malik Teague <malikisiah214@gmail.com>
2023-06-26 09:23:34 -0700

    finished footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,8 @@ import NavBar from "../components/navbar";
 export default function Home() {
   return (
     <div>
-      <div
-        className='hero min-h-screen'
-        // style={{
-        //   backgroundImage: `url("https://w.wallhaven.cc/full/xl/wallhaven-xlrp3z.jpg")`,
-        // }}
-      >
-        <div className='hero-overlay bg-opacity-60'></div>
-        <div className='hero-content text-center text-neutral-content'>
-          <div className='max-w-md'>
-            <h1 className='mb-5 text-5xl font-bold'>Project Carnival</h1>
-            <p className='mb-5'></p>
-          </div>
-        </div>
+      <div className='flex items-center justify-center min-h-screen'>
+        <h1 className='mb-5 text-5xl font-bold text-center'>Carnival Guide</h1>
       </div>
       <Footer />
     </div>
