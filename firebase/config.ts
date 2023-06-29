eecd7dd81a25178563f77355fe492fb8a1663131
Tony Pham <tvpham9@asu.edu>
2023-06-28 21:14:22 -0700

    Firebase authentication and database setup for storing user data

---
+++ firebase/config.ts
@@ -0,0 +1,27 @@
+import * as firebase from "firebase/app";
+import * as auth from "firebase/auth";
+import * as firestore from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseUrl: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+let firebaseApp;
+
+if (!firebase.getApps().length) {
+  firebaseApp = firebase.initializeApp(firebaseConfig);
+} else {
+  firebaseApp = firebase.getApp();
+}
+
+const firebaseAuth = auth.getAuth(firebaseApp);
+const firebaseDb = firestore.getFirestore(firebaseApp);
+
+export { firebaseApp, firebaseAuth, firebaseDb };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
+"use client";
 import Footer from "@/components/footer";
 import NavBar from "../components/navbar";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { firebaseAuth } from "../../firebase/config";
 import DealsComponent from "../components/HotDeals";
 
 export default function Home() {
+  const [user, loading, error] = useAuthState(firebaseAuth);
+  console.log("Loading:", loading, "|", "Current user:", user);
+
   return (
     <div>
-      <div className='flex items-center justify-center min-h-screen'>
-        <h1 className='mb-5 text-5xl font-bold text-center'>Carnival Guide</h1>
+      <div className="flex items-center justify-center min-h-screen">
+        <h1 className="mb-5 text-5xl font-bold text-center">Carnival Guide</h1>
       </div>
       <DealsComponent />
       <Footer />
