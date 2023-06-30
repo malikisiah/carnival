fd74f60f819c26d8c612849600ed5c1a9198daf6
Malik Teague <malikisiah214@gmail.com>
2023-06-30 07:59:20 -0700

    fixed typescript errors

--- src/components/HotDeals.tsx
+++ src/components/HotDeals.tsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function DealsComponent() {
   const [currentSection, setCurrentSection] = useState(40);
 
-  const handleTextLineClick = (section) => {
+  const handleTextLineClick = (section: React.SetStateAction<number>) => {
     setCurrentSection(section);
   };
 
   return (
-    <div style={{ display: 'flex' }}>
+    <div style={{ display: "flex" }}>
       <HotDealsSection />
       <TextLinesSection
-        handleTextLineClick={handleTextLineClick} 
+        handleTextLineClick={handleTextLineClick}
         currentSection={currentSection}
-        />
+      />
       {currentSection === 40 ? (
         <DealsSection40 />
       ) : currentSection === 50 ? (
         <DealsSection50 />
-      ) : currentSection === 60 ?(
+      ) : currentSection === 60 ? (
         <DealsSection60 />
       ) : (
         <DealsSection70 />
@@ -31,56 +31,50 @@ function HotDealsSection() {
   return (
     <div
       style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        background: 'red',
-        padding: '10px',
-        width: '50px',
-        height: '325px',
-        
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "red",
+        padding: "10px",
+        width: "50px",
+        height: "325px",
       }}
     >
       <p
         style={{
-          writingMode: 'vertical-lr',
-          textOrientation: 'upright',
-          color: 'white',
-          fontSize: '24px',
-          margin: '10',
+          writingMode: "vertical-lr",
+          textOrientation: "upright",
+          color: "white",
+          fontSize: "24px",
+          margin: "10",
         }}
       >
-        <span style={{ marginBottom: '8px' }}>H</span>
-        <span style={{ marginBottom: '8px' }}>O</span>
-        T
-        {'\u00A0'}
+        <span style={{ marginBottom: "8px" }}>H</span>
+        <span style={{ marginBottom: "8px" }}>O</span>T{"\u00A0"}
       </p>
       <p
         style={{
-          writingMode: 'vertical-lr',
-          textOrientation: 'upright',
-          color: 'yellow',
-          fontSize: '24px',
-          margin: '0',
+          writingMode: "vertical-lr",
+          textOrientation: "upright",
+          color: "yellow",
+          fontSize: "24px",
+          margin: "0",
         }}
       >
-        <span style={{ marginBottom: '8px' }}>D</span>
-        <span style={{ marginBottom: '8px' }}>E</span>
-        <span style={{ marginBottom: '8px' }}>A</span>
-        <span style={{ marginBottom: '8px' }}>L</span>
-        S
-        
-        
+        <span style={{ marginBottom: "8px" }}>D</span>
+        <span style={{ marginBottom: "8px" }}>E</span>
+        <span style={{ marginBottom: "8px" }}>A</span>
+        <span style={{ marginBottom: "8px" }}>L</span>S
       </p>
     </div>
   );
 }
 
-function TextLinesSection({ handleTextLineClick, currentSection }) {
+function TextLinesSection({ handleTextLineClick, currentSection }: any) {
   const [hoveredLine, setHoveredLine] = useState(null);
 
-  const handleTextLineMouseEnter = (line) => {
+  const handleTextLineMouseEnter = (line: any) => {
     setHoveredLine(line);
   };
 
@@ -91,21 +85,26 @@ function TextLinesSection({ handleTextLineClick, currentSection }) {
   return (
     <div
       style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        background: 'white',
-        padding: '10px',
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "white",
+        padding: "10px",
       }}
     >
-      <div style={{ marginBottom: '25px' }}>
+      <div style={{ marginBottom: "25px" }}>
         <p
           style={{
-            fontSize: '16px',
-            margin: '0',
-            color: currentSection === 40 ? 'red' : hoveredLine === 40 ? 'red' : 'black',
-            cursor: 'pointer',
+            fontSize: "16px",
+            margin: "0",
+            color:
+              currentSection === 40
+                ? "red"
+                : hoveredLine === 40
+                ? "red"
+                : "black",
+            cursor: "pointer",
           }}
           onClick={() => handleTextLineClick(40)}
           onMouseEnter={() => handleTextLineMouseEnter(40)}
@@ -115,20 +114,25 @@ function TextLinesSection({ handleTextLineClick, currentSection }) {
         </p>
         <div
           style={{
-            borderTop: '1px solid black',
-            width: '100%',
-            marginTop: '5px',
-            marginBottom: '5px',
+            borderTop: "1px solid black",
+            width: "100%",
+            marginTop: "5px",
+            marginBottom: "5px",
           }}
         ></div>
       </div>
-      <div style={{ marginBottom: '25px' }}>
+      <div style={{ marginBottom: "25px" }}>
         <p
           style={{
-            fontSize: '16px',
-            margin: '0',
-            color: currentSection === 50 ? 'red' : hoveredLine === 50 ? 'red' : 'black',
-            cursor: 'pointer',
+            fontSize: "16px",
+            margin: "0",
+            color:
+              currentSection === 50
+                ? "red"
+                : hoveredLine === 50
+                ? "red"
+                : "black",
+            cursor: "pointer",
           }}
           onClick={() => handleTextLineClick(50)}
           onMouseEnter={() => handleTextLineMouseEnter(50)}
@@ -138,20 +142,25 @@ function TextLinesSection({ handleTextLineClick, currentSection }) {
         </p>
         <div
           style={{
-            borderTop: '1px solid black',
-            width: '100%',
-            marginTop: '5px',
-            marginBottom: '5px',
+            borderTop: "1px solid black",
+            width: "100%",
+            marginTop: "5px",
+            marginBottom: "5px",
           }}
         ></div>
       </div>
-      <div style={{ marginBottom: '25px' }}>
+      <div style={{ marginBottom: "25px" }}>
         <p
           style={{
-            fontSize: '16px',
-            margin: '0',
-            color: currentSection === 60 ? 'red' : hoveredLine === 60 ? 'red' : 'black',
-            cursor: 'pointer',
+            fontSize: "16px",
+            margin: "0",
+            color:
+              currentSection === 60
+                ? "red"
+                : hoveredLine === 60
+                ? "red"
+                : "black",
+            cursor: "pointer",
           }}
           onClick={() => handleTextLineClick(60)}
           onMouseEnter={() => handleTextLineMouseEnter(60)}
@@ -161,20 +170,25 @@ function TextLinesSection({ handleTextLineClick, currentSection }) {
         </p>
         <div
           style={{
-            borderTop: '1px solid black',
-            width: '100%',
-            marginTop: '5px',
-            marginBottom: '5px',
+            borderTop: "1px solid black",
+            width: "100%",
+            marginTop: "5px",
+            marginBottom: "5px",
           }}
         ></div>
       </div>
       <div>
         <p
           style={{
-            fontSize: '16px',
-            margin: '0',
-            color: currentSection === 70 ? 'red' : hoveredLine === 70 ? 'red' : 'black',
-            cursor: 'pointer',
+            fontSize: "16px",
+            margin: "0",
+            color:
+              currentSection === 70
+                ? "red"
+                : hoveredLine === 70
+                ? "red"
+                : "black",
+            cursor: "pointer",
           }}
           onClick={() => handleTextLineClick(70)}
           onMouseEnter={() => handleTextLineMouseEnter(70)}
@@ -184,10 +198,10 @@ function TextLinesSection({ handleTextLineClick, currentSection }) {
         </p>
         <div
           style={{
-            borderTop: '1px solid black',
-            width: '100%',
-            marginTop: '5px',
-            marginBottom: '5px',
+            borderTop: "1px solid black",
+            width: "100%",
+            marginTop: "5px",
+            marginBottom: "5px",
           }}
         ></div>
       </div>
@@ -199,22 +213,22 @@ function DealsSection40() {
   return (
     <div
       style={{
-        width: '350px',
-        height: '325px',
-        background: 'white',
-        padding: '10px',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
+        width: "350px",
+        height: "325px",
+        background: "white",
+        padding: "10px",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
       }}
     >
       {/* Deal 1 */}
       <div>
         <img
-          src="image1.jpg"
-          alt="Deal 1"
-          style={{ width: '100%', height: 'auto' }}
+          src='image1.jpg'
+          alt='Deal 1'
+          style={{ width: "100%", height: "auto" }}
         />
         <p>Descriptive text for Deal 1</p>
       </div>
@@ -222,9 +236,9 @@ function DealsSection40() {
       {/* Deal 2 */}
       <div>
         <img
-          src="image2.jpg"
-          alt="Deal 2"
-          style={{ width: '100%', height: 'auto' }}
+          src='image2.jpg'
+          alt='Deal 2'
+          style={{ width: "100%", height: "auto" }}
         />
         <p>Descriptive text for Deal 2</p>
       </div>
@@ -232,9 +246,9 @@ function DealsSection40() {
       {/* Deal 3 */}
       <div>
         <img
-          src="image3.jpg"
-          alt="Deal 3"
-          style={{ width: '100%', height: 'auto' }}
+          src='image3.jpg'
+          alt='Deal 3'
+          style={{ width: "100%", height: "auto" }}
         />
         <p>Descriptive text for Deal 3</p>
       </div>
@@ -242,9 +256,9 @@ function DealsSection40() {
       {/* Deal  */}
       <div>
         <img
-          src="image4.jpg"
-          alt="Deal 4"
-          style={{ width: '100%', height: 'auto' }}
+          src='image4.jpg'
+          alt='Deal 4'
+          style={{ width: "100%", height: "auto" }}
         />
         <p>Descriptive text for Deal 4</p>
       </div>
@@ -256,22 +270,22 @@ function DealsSection50() {
   return (
     <div
       style={{
-        width: '350px',
-        height: '325x',
-        background: 'white',
-        padding: '10px',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
+        width: "350px",
+        height: "325x",
+        background: "white",
+        padding: "10px",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
       }}
     >
       {/* Deal 1 */}
       <div>
         <img
-          src="image1.jpg"
-          alt="Deal 1"
-          style={{ width: '100%', height: 'auto' }}
+          src='image1.jpg'
+          alt='Deal 1'
+          style={{ width: "100%", height: "auto" }}
         />
         <p>Descriptive text for Deal 5</p>
       </div>
@@ -279,9 +293,9 @@ function DealsSection50() {
       {/* Deal 2 */}
       <div>
         <img
-          src="image2.jpg"
-          alt="Deal 2"
-          style={{ width: '100%', height: 'auto' }}
+          src='image2.jpg'
+          alt='Deal 2'
+          style={{ width: "100%", height: "auto" }}
         />
         <p>Descriptive text for Deal 6</p>
       </div>
@@ -289,9 +303,9 @@ function DealsSection50() {
       {/* Deal 3 */}
       <div>
         <img
-          src="image3.jpg"
-          alt="Deal 3"
-          style={{ width: '100%', height: 'auto' }}
+          src='image3.jpg'
+          alt='Deal 3'
+          style={{ width: "100%", height: "auto" }}
         />
         <p>Descriptive text for Deal 7</p>
       </div>
@@ -299,9 +313,9 @@ function DealsSection50() {
       {/* Deal 4 */}
       <div>
         <img
-          src="image4.jpg"
-          alt="Deal 4"
-          style={{ width: '100%', height: 'auto' }}
+          src='image4.jpg'
+          alt='Deal 4'
+          style={{ width: "100%", height: "auto" }}
         />
         <p>Descriptive text for Deal 8</p>
       </div>
@@ -313,22 +327,22 @@ function DealsSection60() {
   return (
     <div
       style={{
-        width: '350px',
-        height: '325px',
-        background: 'white',
-        padding: '10px',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
+        width: "350px",
+        height: "325px",
+        background: "white",
+        padding: "10px",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
       }}
     >
       {/* Deal 1 */}
       <div>
         <img
-          src="image1.jpg"
-          alt="Deal 1"
-          style={{ width: '100%', height: 'auto' }}
+          src='image1.jpg'
+          alt='Deal 1'
+          style={{ width: "100%", height: "auto" }}
         />
         <p>Descriptive text for Deal 9</p>
       </div>
@@ -336,9 +350,9 @@ function DealsSection60() {
       {/* Deal 2 */}
       <div>
         <img
-          src="image2.jpg"
-          alt="Deal 2"
-          style={{ width: '100%', height: 'auto' }}
+          src='image2.jpg'
+          alt='Deal 2'
+          style={{ width: "100%", height: "auto" }}
         />
         <p>Descriptive text for Deal 10</p>
       </div>
@@ -346,9 +360,9 @@ function DealsSection60() {
       {/* Deal 3 */}
       <div>
         <img
-          src="image3.jpg"
-          alt="Deal 3"
-          style={{ width: '100%', height: 'auto' }}
+          src='image3.jpg'
+          alt='Deal 3'
+          style={{ width: "100%", height: "auto" }}
         />
         <p>Descriptive text for Deal 11</p>
       </div>
@@ -356,9 +370,9 @@ function DealsSection60() {
       {/* Deal 4 */}
       <div>
         <img
-          src="image4.jpg"
-          alt="Deal 4"
-          style={{ width: '100%', height: 'auto' }}
+          src='image4.jpg'
+          alt='Deal 4'
+          style={{ width: "100%", height: "auto" }}
         />
         <p>Descriptive text for Deal 12</p>
       </div>
@@ -370,22 +384,22 @@ function DealsSection70() {
   return (
     <div
       style={{
-        width: '350px',
-        height: '325px',
-        background: 'white',
-        padding: '10px',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
+        width: "350px",
+        height: "325px",
+        background: "white",
+        padding: "10px",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
       }}
     >
       {/* Deal 1 */}
       <div>
         <img
-          src="image1.jpg"
-          alt="Deal 1"
-          style={{ width: '100%', height: 'auto' }}
+          src='image1.jpg'
+          alt='Deal 1'
+          style={{ width: "100%", height: "auto" }}
         />
         <p>Descriptive text for Deal 13</p>
       </div>
@@ -393,9 +407,9 @@ function DealsSection70() {
       {/* Deal 2 */}
       <div>
         <img
-          src="image2.jpg"
-          alt="Deal 2"
-          style={{ width: '100%', height: 'auto' }}
+          src='image2.jpg'
+          alt='Deal 2'
+          style={{ width: "100%", height: "auto" }}
         />
         <p>Descriptive text for Deal 14</p>
       </div>
@@ -403,9 +417,9 @@ function DealsSection70() {
       {/* Deal 3 */}
       <div>
         <img
-          src="image3.jpg"
-          alt="Deal 3"
-          style={{ width: '100%', height: 'auto' }}
+          src='image3.jpg'
+          alt='Deal 3'
+          style={{ width: "100%", height: "auto" }}
         />
         <p>Descriptive text for Deal 15</p>
       </div>
@@ -413,9 +427,9 @@ function DealsSection70() {
       {/* Deal 4 */}
       <div>
         <img
-          src="image4.jpg"
-          alt="Deal 4"
-          style={{ width: '100%', height: 'auto' }}
+          src='image4.jpg'
+          alt='Deal 4'
+          style={{ width: "100%", height: "auto" }}
         />
         <p>Descriptive text for Deal 16</p>
       </div>
