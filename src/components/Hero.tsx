c641986a8d31eed2670a00b1e4f6d7a51aadf1be
ajaytiwari99 <atiwar22@asu.edu>
2023-07-03 00:08:34 -0700

    fixed hero component style

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,7 @@
 const Hero: React.FC = () => {
   return (
-    <div className="w-full h-screen my-1">
-      <img
-        src="/main-sec-bnr.jpg"
-        alt="Hero Image"
-        className="w-full h-full object-cover"
-      />
+    <div className="relative">
+      <img src="/main-sec-bnr.jpg" alt="Hero Image" className="w-full h-auto" />
     </div>
   );
 };
