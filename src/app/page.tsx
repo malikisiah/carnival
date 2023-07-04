bb8efd58d2a58a626c8656d281c7d629696893f2
Margarita Soleynik <msoleyni@asu.edu>
2023-07-04 02:31:29 -0700

    add carousel and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
+import Carousel from "./carousel";
+import HomepageBanner from "./homepageBanner";
+
 export default function Home() {
   return (
     <div
-      className='hero min-h-screen'
+      className='min-h-screen'
       // style={{
       //   backgroundImage: `url("https://w.wallhaven.cc/full/xl/wallhaven-xlrp3z.jpg")`,
       // }}
     >
-      <div className='hero-overlay bg-opacity-60'></div>
-      <div className='hero-content text-center text-neutral-content'>
-        <div className='max-w-md'>
+      {/* <div className='hero-overlay bg-opacity-60'></div>
+      <div className='hero-content text-center text-neutral-content'> */}
+        {/* <div className='max-w-md'>
           <h1 className='mb-5 text-5xl font-bold'>Project Carnival</h1>
-
           <p className='mb-5'></p>
-        </div>
-      </div>
+        </div> */}
+        <Carousel />
+        <HomepageBanner />
+      {/* </div> */}
     </div>
   );
 }
