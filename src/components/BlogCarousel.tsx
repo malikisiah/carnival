a366df3419737c5a72b5f64dd684d72257fc40f6
ajaytiwari99 <atiwar22@asu.edu>
2023-07-04 20:07:28 -0700

    button functionality online

--- src/components/BlogCarousel.tsx
+++ src/components/BlogCarousel.tsx
@@ -1,91 +1,121 @@
-type BlogDetailsProps = {
-  name: string;
-  date: string;
-  comment: string;
-};
+import React, { useState } from "react";
+import BlogDetails from "./BlogDetails";
 
-const BlogDetails = ({ name, date, comment }: BlogDetailsProps) => {
-  return (
-    <div className="w-full flex flex-col items-center mt-3">
-      <a href="#" className="text-center font-bold text-xs">
-        {name}
-      </a>
-      <div className="flex justify-between w-full">
-        <a href="#" className="text-left font-bold text-xs">
-          {date}
-        </a>
-        <a href="#" className="text-right font-bold text-xs">
-          {comment}
-        </a>
-      </div>
-      <div className="flex justify-end w-full mt-1">
-        <a href="#" className="text-blue-500">
-          Read more
-        </a>
-      </div>
-    </div>
-  );
-};
+const allBlogs = [
+  {
+    name: "Blog 1",
+    date: "Jan 1, 2023",
+    comment: "20 Comments",
+    imgSrc: "https://via.placeholder.com/270x257",
+    alt: "blog1",
+  },
+  {
+    name: "Blog 2",
+    date: "Feb 2, 2023",
+    comment: "10 Comments",
+    imgSrc: "https://via.placeholder.com/270x257",
+    alt: "blog2",
+  },
+  {
+    name: "Blog 3",
+    date: "Mar 3, 2023",
+    comment: "15 Comments",
+    imgSrc: "https://via.placeholder.com/270x257",
+    alt: "blog3",
+  },
+  {
+    name: "Blog 4",
+    date: "Apr 4, 2023",
+    comment: "30 Comments",
+    imgSrc: "https://via.placeholder.com/270x257",
+    alt: "blog4",
+  },
+  {
+    name: "Blog 5",
+    date: "May 5, 2023",
+    comment: "40 Comments",
+    imgSrc: "https://via.placeholder.com/270x257",
+    alt: "blog5",
+  },
+  {
+    name: "Blog 6",
+    date: "Jun 6, 2023",
+    comment: "50 Comments",
+    imgSrc: "https://via.placeholder.com/270x257",
+    alt: "blog6",
+  },
+  {
+    name: "Blog 7",
+    date: "Jul 7, 2023",
+    comment: "60 Comments",
+    imgSrc: "https://via.placeholder.com/270x257",
+    alt: "blog7",
+  },
+  {
+    name: "Blog 8",
+    date: "Aug 8, 2023",
+    comment: "70 Comments",
+    imgSrc: "https://via.placeholder.com/270x257",
+    alt: "blog8",
+  },
+  {
+    name: "Blog 9",
+    date: "Sep 9, 2023",
+    comment: "80 Comments",
+    imgSrc: "https://via.placeholder.com/270x257",
+    alt: "blog9",
+  },
+  // add more blogs here...
+];
 
 const BlogCarousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const blogs = allBlogs.slice(currentIndex, currentIndex + 4);
+
+  const goNext = () => {
+    setCurrentIndex((prevIndex) =>
+      Math.min(prevIndex + 1, allBlogs.length - 4)
+    );
+  };
+
+  const goPrev = () => {
+    setCurrentIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+  };
+
   return (
     <div className="flex justify-center">
-      <div className="blog-carousel bg-red-700 w-auto h-auto">
-        <div className="flex justify-between items-center p-3 mb-5">
-          <span className="block-title bg-green-300 px-4 py-2">
-            FROM THE BLOG
-          </span>
+      <div className="blog-carousel w-auto h-auto">
+        <div className="flex justify-between items-center p-3 mb-2">
+          <span className="block-title px-4 py-2">FROM THE BLOG</span>
           <div className="flex gap-3">
-            <button className="bg-blue-500 text-white px-3 py-1 rounded">
+            <button
+              className="bg-blue-500 text-white px-3 py-1 rounded"
+              onClick={goPrev}
+            >
               Prev
             </button>
-            <button className="bg-blue-500 text-white px-3 py-1 rounded">
+            <button
+              className="bg-blue-500 text-white px-3 py-1 rounded"
+              onClick={goNext}
+            >
               Next
             </button>
           </div>
         </div>
-        <div className="blog-carousel-content bg-blue-300 w-auto h-auto">
+        <div className="blog-carousel-content w-auto h-auto">
           <div className="flex gap-7 px-4 py-3 Blog-item">
-            <div>
-              <a href="#">
-                <img src="https://via.placeholder.com/270x257" alt="blog1" />
-              </a>
-              <BlogDetails
-                name="My name is john wick and you killed my dog"
-                date="Jan 1, 2023"
-                comment="20 Comments"
-              />
-            </div>
-            <div>
-              <a href="#">
-                <img src="https://via.placeholder.com/270x257" alt="blog2" />
-              </a>
-              <BlogDetails
-                name="Blog 2"
-                date="Feb 2, 2023"
-                comment="10 Comments"
-              />
-            </div>
-            <div>
-              <a href="#">
-                <img src="https://via.placeholder.com/270x257" alt="blog3" />
-              </a>
-              <BlogDetails
-                name="Blog 3"
-                date="Mar 3, 2023"
-                comment="15 Comments"
-              />
-            </div>
-            <div>
-              <a href="#">
-                <img src="https://via.placeholder.com/270x257" alt="blog4" />
-              </a>
-              <BlogDetails
-                name="Blog 4"
-                date="Apr 4, 2023"
-                comment="30 Comments"
-              />
-            </div>
+            {blogs.map((blog) => (
+              <div key={blog.name}>
+                <a href="#">
+                  <img src={blog.imgSrc} alt={blog.alt} />
+                </a>
+                <BlogDetails
+                  name={blog.name}
+                  date={blog.date}
+                  comment={blog.comment}
+                />
+              </div>
+            ))}
           </div>
         </div>
       </div>
