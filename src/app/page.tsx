d2d8718848969c12d5a1949e8a9cab0d1e6d2dd6
Margarita Soleynik <msoleyni@asu.edu>
2023-07-05 14:12:57 -0700

    Add Carousel and Banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import NavBar from "../components/navbar";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { firebaseAuth } from "../../firebase/config";
 import DealsComponent from "../components/HotDeals";
+import Carousel from "@/components/carousel";
+import HomepageBanner from "@/components/homepageBanner";
 
 export default function Home() {
   const [user, loading, error] = useAuthState(firebaseAuth);
@@ -14,6 +16,8 @@ export default function Home() {
       <div className="flex items-center justify-center min-h-screen">
         <h1 className="mb-5 text-5xl font-bold text-center">Carnival Guide</h1>
       </div>
+      <Carousel />
+      <HomepageBanner />
       <DealsComponent />
       <Footer />
     </div>
