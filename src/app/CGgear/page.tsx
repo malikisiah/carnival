66f64e08857a099cb073e1880a3f2fb8e9c2ea21
Malik Teague <malikisiah214@gmail.com>
2023-07-05 18:58:23 -0700

    added CG page

---
+++ src/app/CGgear/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex min-h-screen justify-center items-center">
+      <h1 className="text-3xl text-center"> CG Gear </h1>
+    </div>
+  );
+}
