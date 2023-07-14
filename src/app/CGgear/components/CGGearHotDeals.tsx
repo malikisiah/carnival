0f274478b3fba513f52d744cc372b7219bf5e26f
Jeffrey LaFleur <jlafleu1@asu.edu>
2023-07-13 18:42:27 -0700

    Created deals component for CGGear

---
+++ src/app/CGgear/components/CGGearHotDeals.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+
+function CGGearHotdeals() {
+  return (
+    <div
+      style={{
+        width: '300px',
+        height: '2000px',
+        //backgroundColor: 'yellow',
+      }}
+    >
+      <Header />
+      <BlockOne />
+      {/* Other content in CGGearHotdeals */}
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div
+      style={{
+        width: '268px',
+        height: '50px',
+        backgroundColor: '#EEEEEE',
+        marginLeft: '15px',
+        display: 'flex',
+        alignItems: 'center',
+        fontFamily: 'Oswald, sans-serif',
+        fontSize: '12px',
+        fontWeight: 'bold',
+        color: '#333',
+      }}
+    >
+      <span style={{ marginLeft: '23px' }}>HOT DEAL OF THIS WEEK</span>
+    </div>
+  );
+}
+
+
+function BlockOne() {
+  return (
+    <div
+      style={{
+        width: '268px',
+        height: '400px',
+        border: '1px solid #EEEEEE',
+        marginLeft: '15px',
+      }}
+    >
+      <Timer />
+      {/* Other content in BlockOne */}
+    </div>
+  );
+}
+
+function Timer() {
+  return (
+    <div
+      style={{
+        width: '268px',
+        height: '66px',
+        paddingTop: '23px',
+        //backgroundColor: '#EEEEEE',
+        display: 'flex',
+        justifyContent: 'space-evenly',
+      }}
+    >
+      <TimerBox />
+      <TimerBox />
+      <TimerBox />
+      <TimerBox />
+      {/* Content for Timer goes here */}
+    </div>
+  );
+}
+
+function TimerBox() {
+  return (
+    <div style={{ }}>
+      <div
+        style={{
+          width: '42px',
+          height: '34px',
+          backgroundColor: 'red',
+          color: 'white',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <span style={{ fontSize: '16px' }}>00</span>
+      </div>
+      <div style={{ textAlign: 'center' }}>
+        <span style={{ fontSize: '12px', color: 'black' }}>Days</span>
+      </div>
+    </div>
+  );
+}
+
+
+export default CGGearHotdeals;
--- src/app/CGgear/page.tsx
+++ src/app/CGgear/page.tsx
@@ -1,7 +1,16 @@
+import CGGearHotDeals from "./components/CGGearHotDeals";
+
+
 export default function Page() {
   return (
+    <>
+    <CGGearHotDeals/>
     <div className="flex min-h-screen justify-center items-center">
       {<h1 className="text-3xl text-center"> CG Gear </h1>}
     </div>
+    
+    
+    
+    </>
   );
 }
