1306af330cf62346d3f2880687e8a561ddf144a0
Margarita Soleynik <msoleyni@asu.edu>
2023-07-15 17:25:26 -0700

    Add Categories component in CG Gear page

---
+++ src/components/Categories.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+
+type CollectionProps = {
+    subHeading: string;
+    image: string;
+  };
+
+  export default function Categories(props: CollectionProps) {
+    return (
+            <div className="grid grid-rows-3 grid-flow-col gap-2 mr-6 border-2 border-solid border-gray-100 bg-white">
+
+            <div className="row-span-3 my-2 px-6 pt-3"> 
+                <ul className="text-[0.9rem] text-gray-500 marker:text-gray-500 list-disc list-inside">
+                    <a href="#"><li className="pb-1 border-b-2 border-gray-300 hover:text-red-500">Dresses</li></a>
+                    <a href="#"><li className="pt-1 pb-1 border-b-2 border-gray-300 hover:text-red-500">Blouses & Shirts</li></a>
+                    <a href="#"><li className="pt-1 pb-1 border-b-2 border-gray-300 hover:text-red-500">Swimwear</li></a>
+                    <a href="#"><li className="pt-1 pb-1 border-b-2 border-gray-300 hover:text-red-500">Nail Art & Salon</li></a>
+                    <a href="#"><li className="pt-1 pb-1 border-b-2 border-gray-300 hover:text-red-500">Skin Care</li></a>
+                    <a href="#"><li className="pt-1 pb-1 border-b-2 border-gray-300 hover:text-red-500">Beauty Equipment</li></a>
+                    <a href="#"><li className="pt-1 pb-1 border-b-2 border-gray-300 hover:text-red-500">Tattoo & Body Art</li></a>
+                </ul>          
+                <div className="pt-7">
+                    <svg viewBox="0 0 30 1" xmlns="http://www.w3.org/2000/svg">
+                    <line x1="0" x2="6" stroke="gray" />
+                    </svg>
+                    <a href="#"><p className="uppercase text-[0.9rem] text-gray-500 hover:text-red-500">View all</p></a>
+                </div>
+            </div>
+
+            <div className="col-span-2 my-2 h-20 text-right pr-4">
+                <div className="text-[1.5rem] text-gray-400"><span>categories</span></div>
+                <div className="uppercase font-bold text-[1.9rem] text-[#F7450F]">{props.subHeading}</div>
+            </div>
+
+            <div className="row-span-2 col-span-2 -mt-7">
+                <Image src={props.image}
+                    alt=''
+                    width={238}
+                    height={239} 
+                />
+            </div>
+    </div> 
+    );
+}
