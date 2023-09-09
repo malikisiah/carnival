69e37105906c0c47e68a1cea815992b7b9506f05
Malik Teague <malikisiah214@gmail.com>
2023-09-09 14:43:39 -0700

    using diff UI component

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -1,15 +1,23 @@
 import ShopComponent from "./ShopComponent";
 import LatestDeals from "./components/LatestDeals";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { firebaseApp } from "../../../firebase/config";
+import Example from "./FloorProducts";
+import FloorProducts from "./FloorProducts";
 
 async function getData() {
   const db = getFirestore(firebaseApp);
   let res: ShopItem[] = [];
 
-  const query = await getDocs(collection(db, "Hair_Items"));
-
-  query.forEach((doc) => res.push(doc.data() as ShopItem));
+  const q = query(collection(db, "Hair_Items"), orderBy("name"));
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => res.push(doc.data() as ShopItem));
 
   return res;
 }
@@ -17,11 +25,10 @@ async function getData() {
 export default async function Page() {
   const items = await getData();
 
-  getData();
-
   return (
     <>
-      <ShopComponent header="HAIR" items={items} />
+      <FloorProducts />
+      {/* <ShopComponent header="HAIR" items={items} /> */}
       <LatestDeals />
     </>
   );
