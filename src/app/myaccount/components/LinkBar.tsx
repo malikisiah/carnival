b9ed7f99496430571435b2471b891550e02f35e6
Malik Teague <malikisiah214@gmail.com>
2023-09-24 09:15:38 -0700

    fixed preview error, once more

--- src/app/myaccount/components/LinkBar.tsx
+++ src/app/myaccount/components/LinkBar.tsx
@@ -4,7 +4,6 @@ import DashboardContent from "./Dashboard";
 import AddressContent from "./Addresses";
 import AccountDetailsContent from "./AccountDetails";
 
-
 export default function LinkBar() {
   // Define the available sections
   const sections = {
@@ -21,15 +20,14 @@ export default function LinkBar() {
 
   const [currentSection, setCurrentSection] = useState("dashboard"); // Set the initial section
 
-  const handleTextLineClick = (section : string) => {
+  const handleTextLineClick = (section: string) => {
     setCurrentSection(section);
   };
 
   return (
     <div className="flex justify-start mt-12">
-      <div style={{ marginRight: "15px", }}>
+      <div style={{ marginRight: "15px" }}>
         {/* Render the first component with 15px margin to the right */}
-        
       </div>
       <div style={{ marginLeft: "15px", marginRight: "15px" }}>
         <Links
@@ -54,96 +52,94 @@ export default function LinkBar() {
 
 // Define the LinksProps type
 interface LinksProps {
-    sections: Record<string, string>;
-    currentSection: string;
-    handleTextLineClick: (section: string) => void;
-  }
+  sections: Record<string, string>;
+  currentSection: string;
+  handleTextLineClick: (section: string) => void;
+}
 
 function Links({ sections, currentSection, handleTextLineClick }: LinksProps) {
-    const linkBarStyle = {
-      backgroundColor: 'lightgrey',
-      display: 'flex',
-      flexDirection: 'column', // Display links vertically
-      alignItems: 'flex-start', // Align text to the left
-      padding: '10px 0',
-      paddingLeft: '15px', // Add 15px padding to the left
-      width: '200px', // Set the width to 200 pixels
-    };
-  
-    const linkStyle = {
-      textDecoration: 'none',
-      color: '#333',
-      fontSize: '14px',
-      margin: '5px 0', // Add 5px space between links vertically
-      cursor: 'pointer',
-      padding: '0 0 5px 0',
-      marginLeft: '20px',
-    };
-  
-    return (
-      <div style={linkBarStyle}>
-        {Object.keys(sections).map((section) => (
-          <a
-            key={section}
-            href="#"
-            style={{
-              ...linkStyle,
-              textDecoration: currentSection === section ? 'underline' : 'none',
-            }}
-            onClick={() => handleTextLineClick(section)}
-          >
-            {sections[section]}
-          </a>
-        ))}
-      </div>
-    );
-  }
-  function Dashboard() {
-      return (
-        <div className='flex min-h-screen'>
-            <DashboardContent />
-        </div>
-      );
-  }
-  
-  
-  function Orders() {
-    return <div>Orders Component</div>;
-  }
-  
-  function Downloads() {
-    return <div>Downloads Component</div>;
-  }
-  
-  function Addresses() {
-    return (
-        <div className='flex min-h-screen'>
-            <AddressContent />
-        </div>
-      );
-  }
-  
-  function AccountDetails() {
-    return (
-      <div className='flex min-h-screen'>
-        <AccountDetailsContent /> {/* Render the component */}
-      </div>
-    );
-  }
+  const linkBarStyle: any = {
+    backgroundColor: "lightgrey",
+    display: "flex",
+    flexDirection: "column", // Display links vertically
+    alignItems: "flex-start", // Align text to the left
+    padding: "10px 0",
+    paddingLeft: "15px", // Add 15px padding to the left
+    width: "200px", // Set the width to 200 pixels
+  };
 
-  
-  function RMARequests() {
-    return <div>RMA Requests Component</div>;
-  }
-  
-  function Vendors() {
-    return <div>Vendors Component</div>;
-  }
-  
-  function SellerSupportTickets() {
-    return <div>Seller Support Tickets Component</div>;
-  }
-  
-  function Logout() {
-    return <div>Logout Component</div>;
-  }
+  const linkStyle = {
+    textDecoration: "none",
+    color: "#333",
+    fontSize: "14px",
+    margin: "5px 0", // Add 5px space between links vertically
+    cursor: "pointer",
+    padding: "0 0 5px 0",
+    marginLeft: "20px",
+  };
+
+  return (
+    <div style={linkBarStyle}>
+      {Object.keys(sections).map((section) => (
+        <a
+          key={section}
+          href="#"
+          style={{
+            ...linkStyle,
+            textDecoration: currentSection === section ? "underline" : "none",
+          }}
+          onClick={() => handleTextLineClick(section)}
+        >
+          {sections[section]}
+        </a>
+      ))}
+    </div>
+  );
+}
+function Dashboard() {
+  return (
+    <div className="flex min-h-screen">
+      <DashboardContent />
+    </div>
+  );
+}
+
+function Orders() {
+  return <div>Orders Component</div>;
+}
+
+function Downloads() {
+  return <div>Downloads Component</div>;
+}
+
+function Addresses() {
+  return (
+    <div className="flex min-h-screen">
+      <AddressContent />
+    </div>
+  );
+}
+
+function AccountDetails() {
+  return (
+    <div className="flex min-h-screen">
+      <AccountDetailsContent /> {/* Render the component */}
+    </div>
+  );
+}
+
+function RMARequests() {
+  return <div>RMA Requests Component</div>;
+}
+
+function Vendors() {
+  return <div>Vendors Component</div>;
+}
+
+function SellerSupportTickets() {
+  return <div>Seller Support Tickets Component</div>;
+}
+
+function Logout() {
+  return <div>Logout Component</div>;
+}
